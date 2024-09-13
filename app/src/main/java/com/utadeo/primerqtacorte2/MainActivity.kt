685242a8d5e29bb82388f28a469632cc54ac3642
Fredy Laverde <fredys.laverdem@uatdeo.edu.co>
2024-09-12 20:56:45 -0500

    Final

--- app/src/main/java/com/utadeo/primerqtacorte2/MainActivity.kt
+++ app/src/main/java/com/utadeo/primerqtacorte2/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.utadeo.primerqtacorte2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var showToast: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var prestamo = findViewById<EditText>(R.id.editTextNumber1)
+        var tasa= findViewById<EditText>(R.id.editTextNumber3)
+        var tiempo= findViewById<EditText>(R.id.editTextNumber4)
+        var tae = findViewById<EditText>(R.id.editTextNumber5)
+
+        val operacionTae = (tae/12)/100
+
+        showToast = findViewById<Button>(R.id.button2)
+
+        showToast.setOnClickListener{
+            Toast.makeText(applicationContext,"Variable resultado", Toast.LENGTH_LONG).show()
+
         }
     }
 }
